e89ba80c39ccd54e39564991ba73299ab132eab3
VictorCam <campav@oregonstate.edu>
2020-03-12 11:14:49 -0700

    implmeneted K-nearest Neighbour
    finished algorithm that was researched

--- TSP.cpp
+++ TSP.cpp
@@ -4,6 +4,9 @@
 #include <vector>
 #include <stdio.h>
 #include <bits/stdc++.h>
+#include <math.h>
+#include <stdio.h>
+#include <algorithm>
 
 using namespace std;
 
@@ -20,7 +23,7 @@ void open_store_file(int argc, char const* argv[], int* file_count, vector<int> 
   else {
     data.open(argv[1]); //the second argument in cmd line will be executed to open
     if(!data) { //check if the file exists
-      cout << "could not open file" << endl; //print an error if the values could not be opened
+      cout << "could not open file [check if the file exist or you mistyped the name]" << endl; //print an error if the values could not be opened
       exit(1);
     }
     else {
@@ -40,16 +43,55 @@ void open_store_file(int argc, char const* argv[], int* file_count, vector<int> 
   }
 }
 
-void TSP(int* file_count, vector<int> &number, vector<int> &xcoord, vector<int> &ycoord) {
+
+void TSP(int argc, char const* argv[], int file_count, vector<int> &number, vector<int> &xcoord, vector<int> &ycoord) {
   ofstream results; //variable declared to create and write to a file
-  cout << "TSP Function Called" << endl;
+  string first_arg = argv[1]; //converting argv[1] to a string
+  string final_str = first_arg + ".tour"; //add .tour to the end of the file
+  results.open(final_str.c_str()); //convert the string into a c string
+  int dis_trav = 0; //will contain the total distance of the traveled path
+  int traveled[file_count]; //will contain the city identifier values
+  vector<int> iden; //will be used for identifying the city value in the nearest vector
+  vector<int> nearest; //will be used for finding which vector has the smallest distance
+  int visited = 0; //check if a value is visited
+
+  traveled[visited] = number[0]; //check if the
+  number[traveled[visited]] = -1;
+
+for (int i = 0; i < file_count; i++) {
+  for (int b = 0; b < file_count; b++) {
+        if(-1 == number[b]) { //values that are -1 are already visited
+          //do nothing
+        }
+        else {
+        int operation1 = pow((xcoord[traveled[visited]] - xcoord[number[b]]), 2.0); //calculating euclid distance
+        int operation2 = pow((ycoord[traveled[visited]] - ycoord[number[b]]), 2.0); //calculating euclid distance
+        nearest.push_back(sqrt(operation1 + operation2)); //calculating euclid distance
+        iden.push_back(number[b]); //the city identifers should be at the same push_backed value
+      }
+    }
+       vector<int>::iterator min = min_element(nearest.begin(), nearest.end());
+       int dis = distance(nearest.begin(), min);
+
+       visited++; //increment the visited value to store its next minimum element
+       traveled[visited] = iden[dis]; //using the position of the dis value to get that particular value
+       number[traveled[visited]] = -1; //mark the city as visited
+       dis_trav += nearest[dis]; //add the value we choose to our total distance traveled
+       nearest.clear(); //clear the vector
+       iden.clear(); //clear the vector
+    }
+    results << dis_trav << endl; //print to full distance cities into the .tour file
+
+    for (int i = 0; i < file_count; i++) {
+      results << traveled[i] << endl; //print the connected cities into the .tour file
+    }
 }
 
 int main(int argc, char const* argv[]) {
   int file_count = 0;
   vector<int> number; vector<int> xcoord; vector<int> ycoord;
   open_store_file(argc, argv, &file_count, number, xcoord, ycoord);
-  TSP(&file_count, number, xcoord, ycoord);
+  TSP(argc, argv, file_count, number, xcoord, ycoord);
 
   return 0;
 }
